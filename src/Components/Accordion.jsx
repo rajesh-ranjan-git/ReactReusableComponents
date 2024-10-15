225d1eff02960b0454648d9ba68ff971b723ca56
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2024-10-15 12:06:55 +0530

    Accordion and Dropdown

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import GoHome from "./GoHome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+const data = [
+  {
+    question: "Question1",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolor dolore neque obcaecati repudiandae porro? Labore, quasi quisquam natus fugit possimus quia sit perferendis! Nemo, repudiandae dolorum. Sit, quas saepe? Ea voluptate, dolore laborum veritatis vitae illo blanditiis fuga at, aspernatur a ut quaerat consequatur esse? Iure illum porro, magnam accusamus aperiam iusto omnis cumque assumenda, earum non voluptate labore.",
+  },
+  {
+    question: "Question2",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolor dolore neque obcaecati repudiandae porro? Labore, quasi quisquam natus fugit possimus quia sit perferendis! Nemo, repudiandae dolorum. Sit, quas saepe? Ea voluptate, dolore laborum veritatis vitae illo blanditiis fuga at, aspernatur a ut quaerat consequatur esse? Iure illum porro, magnam accusamus aperiam iusto omnis cumque assumenda, earum non voluptate labore.",
+  },
+  {
+    question: "Question3",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolor dolore neque obcaecati repudiandae porro? Labore, quasi quisquam natus fugit possimus quia sit perferendis! Nemo, repudiandae dolorum. Sit, quas saepe? Ea voluptate, dolore laborum veritatis vitae illo blanditiis fuga at, aspernatur a ut quaerat consequatur esse? Iure illum porro, magnam accusamus aperiam iusto omnis cumque assumenda, earum non voluptate labore.",
+  },
+  {
+    question: "Question4",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolor dolore neque obcaecati repudiandae porro? Labore, quasi quisquam natus fugit possimus quia sit perferendis! Nemo, repudiandae dolorum. Sit, quas saepe? Ea voluptate, dolore laborum veritatis vitae illo blanditiis fuga at, aspernatur a ut quaerat consequatur esse? Iure illum porro, magnam accusamus aperiam iusto omnis cumque assumenda, earum non voluptate labore.",
+  },
+];
 
 const Accordion = () => {
+  const [acc, setAcc] = useState(null);
+
+  const handleAccordion = (index) => {
+    if (acc === index) {
+      setAcc(null);
+    } else {
+      setAcc(index);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col items-center h-screen">
         <h1 className="text-5xl p-5">Accordion</h1>
+        {data.map((ques, index) => {
+          return (
+            <div className="w-[90%]" key={index}>
+              <div
+                className="flex justify-between m-4 p-4 text-white text-2xl bg-lime-600 font-semibold rounded-md shadow-xl shadow-slate-950 cursor-pointer"
+                onClick={() => handleAccordion(index)}
+              >
+                <span>{ques.question}</span>
+                {acc === index ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
+              </div>
+              {acc === index && (
+                <div className="flex justify-between m-4 p-4 text-lime-600 text-2xl bg-lime-200 font-semibold rounded-md shadow-xl shadow-slate-950">
+                  <span>{ques.answer}</span>
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
       <GoHome />
     </>
--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import GoHome from "./GoHome";
 
+const countries = [
+  { name: "India", cities: ["Delhi", "Mumbai"] },
+  { name: "Pakistan", cities: ["Lahore", "Islamabad"] },
+  { name: "Bangladesh", cities: ["Dhaka", "Khulna"] },
+  { name: "Sri Lanka", cities: ["Colombo", "Galle"] },
+];
+
 const Dropdown = () => {
+  const [selectedCountry, setSelectedCountry] = useState(0);
+
+  const handleDropdown = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
   return (
     <>
       <div className="flex flex-col items-center h-screen">
-        <h1 className="text-5xl p-5">Dropdown</h1>
+        <h1 className="text-5xl m-5 p-5">Dropdown</h1>
+        <div>
+          {/* 1st DropDown */}
+          <select
+            className="p-3 m-2 w-96 text-center text-2xl outline-none bg-white border-4 border-lime-600 rounded-md"
+            onChange={(e) => handleDropdown(e)}
+          >
+            {countries.map((country, index) => {
+              return (
+                <option value={index} key={index}>
+                  {country.name}
+                </option>
+              );
+            })}
+          </select>
+
+          {/* 2nd DropDown */}
+          <select
+            className="p-3 w-96 text-center text-2xl outline-none bg-white border-4 border-lime-600 rounded-md"
+            placeholder="Search Here..."
+          >
+            {countries[selectedCountry].cities.map((city, index) => {
+              return (
+                <option value={city} key={index}>
+                  {city}
+                </option>
+              );
+            })}
+          </select>
+        </div>
       </div>
       <GoHome />
     </>
