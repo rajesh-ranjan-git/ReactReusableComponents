285c1886bb3e7b1cc5e38a01d5ab5ba15f1c61ec
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2025-11-16 12:58:25 +0530

    working with css help

--- src/Components/Dropdown2/Dropdown2.jsx
+++ src/Components/Dropdown2/Dropdown2.jsx
@@ -6,6 +6,104 @@ const Dropdown2 = () => {
     <>
       <div className="flex flex-col items-center">
         <h1 className="m-5 p-5 text-5xl">Dropdown Menus</h1>
+        <div className="flex flex-col justify-between p-2 w-screen h-[70vh] text-white">
+          <div className="flex justify-between">
+            <div>
+              <button
+                popovertarget="menu-1"
+                className="bg-lime-800 p-2 rounded-lg font-semibold"
+              >
+                Dropdown Menu 1
+              </button>
+              <div
+                id="menu-1"
+                className="absolute bg-lime-500 my-2 p-1 rounded-lg w-max text-white menu"
+                popover=""
+              >
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Longer Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Very Long Item Name
+                </a>
+              </div>
+            </div>
+            <div>
+              <button
+                popovertarget="menu-2"
+                className="bg-lime-800 p-2 rounded-lg font-semibold"
+              >
+                Dropdown Menu 2
+              </button>
+              <div
+                id="menu-2"
+                className="absolute bg-lime-500 my-2 p-1 rounded-lg w-max text-white menu"
+                popover=""
+              >
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Longer Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Very Long Item Name
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <button
+                popovertarget="menu-3"
+                className="bg-lime-800 p-2 rounded-lg font-semibold"
+              >
+                Dropdown Menu 3
+              </button>
+              <div
+                id="menu-3"
+                className="absolute bg-lime-500 my-2 p-1 rounded-lg w-max text-white menu"
+                popover=""
+              >
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Longer Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Very Long Item Name
+                </a>
+              </div>
+            </div>
+            <div>
+              <button
+                popovertarget="menu-4"
+                className="bg-lime-800 p-2 rounded-lg font-semibold"
+              >
+                Dropdown Menu 4
+              </button>
+              <div
+                id="menu-4"
+                className="absolute bg-lime-500 my-2 p-1 rounded-lg w-max text-white menu"
+                popover=""
+              >
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Longer Item
+                </a>
+                <a href="" className="hover:bg-lime-700 p-2 rounded-lg">
+                  Very Long Item Name
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <GoHome />
     </>
